ba37431a25bdf03d887598f969ea71d00efd8353
Revathi Chandana <chandanarevathi30@gmail.com>
2020-06-13 21:05:54 +0530

    uc3:store the doublet combination as well as the percentage

--- flip_coin_combination.sh
+++ flip_coin_combination.sh
@@ -6,32 +6,21 @@ tail=1
 head_count=0
 tail_count=0
 count=0
-declare -A doublet=( [HH]=0 [HT]=0 [TH]=0 [TT]=0 )
+declare -A doublet=( [00]=0 [01]=0 [10]=0 [11]=0 )
 while [ $count -lt 10 ]
 do
 	first_coin=$((head+$((RANDOM%diff))))
 	second_coin=$((head+$((RANDOM%diff))))
-	if [[ $first_coin -eq 0 && $second_coin -eq 0 ]]
-	then
-		doublet[HH]=$((${doublet[HH]}+1))
-	elif [[ $first_coin -eq 0 && $second_coin -eq 1 ]]
-	then
-		doublet[HT]=$((${doublet[HT]}+1))
-        elif [[ $first_coin -eq 1 && $second_coin -eq 0 ]]
-        then
-                doublet[TH]=$((${doublet[TH]}+1))
-	else
-		doublet[TT]=$((${doublet[TT]}+1))
-	fi
+	doublet[$first_coin$second_coin]=$((${doublet[$first_coin$second_coin]}+1))
 ((count++))
 done
 echo ${!doublet[@]}
 echo ${doublet[@]}
-per_HH=`echo ${doublet[HH]} $count | awk '{ print ($1/$2)*100 }'`
-per_HT=`echo ${doublet[HT]} $count | awk '{ print ($1/$2)*100 }'`
-per_TH=`echo ${doublet[TH]} $count | awk '{ print ($1/$2)*100 }'`
-per_TT=`echo ${doublet[TT]} $count | awk '{ print ($1/$2)*100 }'`
-echo "percentage of the doublet combination of HH : $per_HH%"
-echo "percentage of the doublet combination of HH : $per_HT%"
-echo "percentage of the doublet combination of HH : $per_TH%"
-echo "percentage of the doublet combination of HH : $per_TT%"
+declare -a key_array=(${!doublet[@]})
+declare -a key_value=(${doublet[@]})
+for (( i=0; i<${#key_array[@]}; i++ ))
+do
+	percentage=`echo ${key_value[$i]} $count | awk '{ print ($1/$2)*100 }'`
+	echo "percentage of the doublet combination of ${key_array[$i]} : $percentage"
+done
+
